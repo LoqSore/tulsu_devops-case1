cca028378fe5c3b72b9e27e63242a82de87e0f05
LoqSore <168979345+LoqSore@users.noreply.github.com>
2025-06-07 03:58:15 +0300

    flask-приложение

---
+++ app.py
@@ -0,0 +1,12 @@
+﻿from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello, world!", 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=6996)
